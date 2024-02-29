8a93564a2dd0185f3aff900b3867225a54d8aa40
Artur Valle <atur.n.valle@gmail.com>
2024-02-29 20:19:07 -0300

    Add function to call area and circ calculation methods

--- Aula-28_02_24/Exercicio2/main.cpp
+++ Aula-28_02_24/Exercicio2/main.cpp
@@ -14,8 +14,8 @@ class Circle{
         float x_center;
         float y_center;
 
-        void calculateArea(){float area = M_PI*radius*radius;}
-        void calculateCirc(){float circ = 2*M_PI*radius;}
+        void calculateArea(){area = M_PI*radius*radius;}
+        void calculateCirc(){circ = 2*M_PI*radius;}
         float calculateDistance(float x2, float y2){return sqrt(pow((x2 - x_center), 2) + pow((y2 - y_center), 2));}
 
     public:
@@ -30,30 +30,42 @@ class Circle{
         void printRadius(){cout << "Radius: " << radius << endl;}
         void printCenter(){cout << "X center: " << x_center << endl << "Y center: " << y_center << endl;}
         void printArea(){cout << "Area: "<< area << endl;}
+        void calculateAreaAndCirc(){
+            calculateArea();
+            calculateCirc();
+        }
         float calculateCenterDist(const Circle otherCircle){return calculateDistance(otherCircle.x_center, otherCircle.y_center);}    
-};
+};      
 
 int main()
 {
     Circle circle1;
     cout << "Circle 1:" << endl;
     circle1.setRadius(3);
+    circle1.calculateAreaAndCirc();
     circle1.printRadius();
+    circle1.printArea();
     circle1.setCenter(3, 4);
     circle1.printCenter();
     circle1.increaseRadiusByPercentage(10);
+    circle1.calculateAreaAndCirc();
     circle1.printRadius();
+    circle1.printArea();
 
     cout << endl;
     cout << "Circle 2:";
 
     Circle circle2;
     circle2.setRadius(2);
+    circle2.calculateAreaAndCirc();
     circle2.printRadius();
+    circle2.printArea();
     circle2.setCenter(0, 0);
     circle2.printCenter();
     circle2.increaseRadiusByPercentage(50);
+    circle2.calculateAreaAndCirc();
     circle2.printRadius();
+    circle2.printArea();
     
     float dist = circle1.calculateCenterDist(circle2);
 
